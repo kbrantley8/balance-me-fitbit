e1f6dd4d66a157b8ef965f30d8e0a1a26b090d42
mgomez <mgomez46@gatech.edu>
2020-07-16 15:55:19 -0400

    screen with list added

--- app/index.js
+++ app/index.js
@@ -3,15 +3,16 @@ import document from "document";
 let homeScreen = document.getElementById("home-screen");
 let presetTasks =  document.getElementById("quickstart-tasks");
 
-presetTasks.display = 'none';
-homeScreen.display = 'inline';
+// add tasks button
+let addTasks = document.getElementById("add-task");
+addTasks.onactivate = function(evt) {
+    presetTasks.style.display = "inline";
+    homeScreen.style.display = "none";
+}
 
-// function showScreen() {
-//     presetTasks.style.display = "inline";
-//     homeScreen.style.display = "none";
-//   }
-
-// let addTasks = document.getElementById("add-task");
-// addTasks.onClick = function() {
-//   showScreen();
-// }
+// back button
+let backButton = document.getElementById('back-button');
+backButton.onactivate = function(evt) {
+    presetTasks.style.display = "none";
+    homeScreen.style.display = "inline";
+}
