2d67822c16f7472a8d642268ef2ef956c982c09d
mgomez <mgomez46@gatech.edu>
2020-07-17 00:12:20 -0400

    added time picker

--- app/index.js
+++ app/index.js
@@ -3,16 +3,22 @@ import document from "document";
 // screens
 let homeScreen = document.getElementById("home-screen");
 let presetTasks =  document.getElementById("quickstart-tasks");
+let taskDetail = document.getElementById("task-detail-screen");
+let timePicker = document.getElementById("tumbler");
 
+function showScreen(string) {
+   (string === 'home') ? homeScreen.style.display = "inline" : homeScreen.style.display = "none";
+   (string === 'preset') ? presetTasks.style.display = "inline" : presetTasks.style.display = "none";
+   (string === 'detail') ? taskDetail.style.display = "inline" : taskDetail.style.display = "none";
+   (string === 'time') ? timePicker.style.display = "inline" : timePicker.style.display = "none";
+}
 
 /********** MY TASKS SCREEN ************/
 // add tasks button
 let addTasks = document.getElementById("add-task");
 addTasks.onactivate = function(evt) {
-    presetTasks.style.display = "inline";
-    homeScreen.style.display = "none";
+    showScreen('preset');
 }
-
 
 /********** QUICKSTART TASKS SCREEN ************/
 let list = document.getElementById("my-list");
@@ -22,12 +28,12 @@ items.forEach((element, index) => {
   let touch = element.getElementById("touch-me");
   touch.onclick = (evt) => {
     console.log(`touched: ${index}`);
+    showScreen('detail');
   }
 });
-// back button
+// back button (does not work rn, only when you remove the footer use)
 let backButton = document.getElementById('back-button');
-backButton.onactivate = function(evt) {
-    presetTasks.style.display = "none";
-    homeScreen.style.display = "inline";
-    console.log('back button clicked');
+backButton.onclick = function(evt) {
+    showScreen('home');
 }
+
